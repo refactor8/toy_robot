78f3d3d06fb871075fe329995ebda535bcb159b2
Ryan Flores <rfloper@icloud.com>
2018-04-12 03:30:01 +0200

    Add the move command

---
+++ lib/toy_robot/robot.rb
@@ -0,0 +1,13 @@
+module ToyRobot
+  class Robot
+    attr_reader :position
+    
+    def initialize(position = 0)
+      @position = position
+    end
+
+    def move
+      @position += 1
+    end
+  end
+end
